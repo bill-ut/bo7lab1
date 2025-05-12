6647f15a750acddbc85e5e0612a0995cf972c87d
Zekorei <46651082+Zekorei@users.noreply.github.com>
2025-05-12 13:48:24 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,51 @@
+public class Polynomial {
+    double[] coefficients;
+
+    public Polynomial() {
+        coefficients = new double[]{0};
+    }
+
+    public Polynomial(double[] coefficients) {
+        this.coefficients = coefficients;
+    }
+
+    // testing function; ignore for evaluation
+    public void print() {
+        for (int i = 0; i < coefficients.length; i++) {
+            System.out.print(coefficients[i] + "x^" + i);
+
+            if (i < coefficients.length - 1)
+                System.out.print(" + ");
+        }
+
+        System.out.print("\n");
+    }
+
+    public Polynomial add(Polynomial other) {
+        if (coefficients.length < other.coefficients.length) {
+            return other.add(this);
+        }
+
+        for (int i = 0; i < coefficients.length; i++) {
+            if (i < other.coefficients.length) {
+                coefficients[i] += other.coefficients[i];
+            }
+        }
+
+        return this;
+    }
+
+    public double evaluate(double x) {
+        double sum = 0.0;
+
+        for (int i = 0; i < coefficients.length; i++) {
+            sum += coefficients[i] * Math.pow(x, i);
+        }
+
+        return sum;
+    }
+
+    public boolean hasRoot(double x) {
+        return this.evaluate(x) == 0;
+    }
+}
